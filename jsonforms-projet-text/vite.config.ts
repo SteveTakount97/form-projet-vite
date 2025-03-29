227f63e92dfa4fa819cb7b6a16f567e8e2ae8a80
steve <stevetak@outlook.fr>
2025-03-29 16:38:30 +0100

    edit gitpage

--- jsonforms-projet-text/vite.config.ts
+++ jsonforms-projet-text/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Configuration pour le déploiement sur GitHub Pages
+  base: '/form-projet-vite/', 
+
   plugins: [react()],
-})
+
+  build: {
+    outDir: 'dist', 
+  },
+});
